05608db8581def52e52c287a028b789952989a1f
José da Silva <josesilva23p@gmail.com>
2023-07-02 12:20:21 +0100

    refactoring the whole project

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -12,20 +12,24 @@ const Explore = () => {
   const [active, setActive] = useState('world-2');
 
   return (
-    <section className={`${styles.paddings}`} id="explore">
+    <section className={`${styles.paddings}`} id='explore'>
       <motion.div
         variants={staggerContainer}
-        initial="hidden"
-        whileInView="show"
+        initial='hidden'
+        whileInView='show'
         viewport={{ once: false, amount: 0.25 }}
         className={`${styles.innerWidth} mx-auto flex flex-col`}
       >
-        <TypingText title="| The World" textStyles="text-center" />
         <TitleText
-          title={<>Choose the world you want <br className="md:block hidden" /> to explore</>}
-          textStyles="text-center"
+          title={
+            <>
+              Choose the world you want <br className='md:block hidden' /> to
+              explore
+            </>
+          }
+          textStyles='text-center'
         />
-        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+        <div className='mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5'>
           {exploreWorlds.map((world, index) => (
             <ExploreCard
               key={world.id}
--- sections/index.js
+++ sections/index.js
@@ -2,8 +2,6 @@ import Hero from './Hero';
 import About from './About';
 import Explore from './Explore';
 import GetStarted from './GetStarted';
-import WhatsNew from './WhatsNew';
-import World from './World';
 import Insights from './Insights';
 import Feedback from './Feedback';
 
@@ -12,8 +10,6 @@ export {
   About,
   Explore,
   GetStarted,
-  WhatsNew,
-  World,
   Insights,
   Feedback,
 };
